51b9fc255772d3da3643f9ee41e8fceff79da7bd
Dindaaa <dindarosalin24@gmail.com>
2023-12-30 15:18:33 +0700

    done hero

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,31 +1,30 @@
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
+import { Container, Nav, Navbar, Button } from 'react-bootstrap';
 import './styles/header.css'
 
 function header() {
   return (
-    <Navbar collapseOnSelect expand="sm" 
-    className="position-fixed rounded-pill mt-4 px-4 border mx-auto">
-      <div className='d-flex justify-content-center gap-4'>
+    <Container>
+      <Navbar collapseOnSelect expand="sm" 
+      className="position-fixed rounded-pill mt-4 px-4 border mx-auto">
+      <div className='d-flex align-items-center'>
         <Navbar.Brand href="/" className='pe-5'>
           <img src="src/assets/navbar-brand.png" 
-          height="20"
-          className="d-inline-block align-top"
+          className="d-flex align-items-center"
           alt="Navbar Brand" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="me-auto gap-5">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#tentang-acara">Tentang Acara</Nav.Link>
             <Nav.Link href='#galeri'>Galeri</Nav.Link>
             <Nav.Link href='#berita'>Berita</Nav.Link>
+            <Button variant="primary" className='rounded-pill'>Primary</Button>{' '}
           </Nav>
-          <Button variant="primary" className='rounded-pill'>Primary</Button>{' '}
-        </Navbar.Collapse>
+          </Navbar.Collapse>
       </div>
     </Navbar>
+    </Container>
   );
 }
 
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,12 +1,19 @@
 import './styles/hero.css'
-import { Button } from 'react-bootstrap'
+import { Button, Image, Container } from 'react-bootstrap'
 
-const hero = () => {
+const Hero = () => {
   return (
-    <div className="hero-img position-relative align-items-center">
-      <Button>ini Button</Button>
+    <div className="hero-img ">
+      <Container className='d-lg-flex align-items-start justify-content-start mb-5'>
+        <div className="mb-5 d-flex flex-column">
+          <Image src="src/assets/logo-event 2.png" className='logo-img' />
+          <div className='mb-5'>
+            <Button className='rounded-pill mb-5'>ini Button</Button>
+          </div>
+        </div>
+      </Container>
     </div>
   )
 }
 
-export default hero
+export default Hero
